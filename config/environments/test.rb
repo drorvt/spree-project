eb773a5b1bd44d65311c27781b2a04e1f4fae9db
drorvt <33095803+drorvt@users.noreply.github.com>
2018-04-29 13:07:39 +0300

    Update test.rb
      # config/application.rb
    
      config.web_console.whitelisted_ips = '37.142.226.148'
      # or a whole network
      config.web_console.whitelisted_ips = '37.142.0.0/16'

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.web_console.whiny_requests = false
+  # config/application.rb
 
+  config.web_console.whitelisted_ips = '37.142.226.148'
+  # or a whole network
+  config.web_console.whitelisted_ips = '37.142.0.0/16'
 end
