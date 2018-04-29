bdadb012cf6c86f6995b30184b1bd5ad1fae41b6
drorvt <33095803+drorvt@users.noreply.github.com>
2018-04-29 12:45:54 +0300

    Update development.rb
      config.web_console.whiny_requests = false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  Rails.application.configure do
   config.web_console.whiny_requests = false
-  end
+
 end
