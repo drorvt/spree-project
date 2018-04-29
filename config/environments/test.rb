98a2736ac8c0d452d9e275fc712c50ed5cd7b22b
drorvt <33095803+drorvt@users.noreply.github.com>
2018-04-29 12:56:03 +0300

    Update test.rb
     config.action_cart_link.disable_request_forgery_protection = true

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,6 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.web_console.whiny_requests = false
+  config.action_cart_link.disable_request_forgery_protection = true
 
 end
